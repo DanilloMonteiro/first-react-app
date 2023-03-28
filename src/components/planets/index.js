0922788bc6836be2594d0d4b2aa2aaa1ce43dc1d
Danillo Gustavo Monteiro <monteirod@brascabos.intranet>
2023-03-28 13:38:15 -0300

    first commit

---
+++ src/components/planets/index.js
@@ -0,0 +1,13 @@
+import React, { fragment } from "react";
+import Planet from "./planet";
+
+const Planets = () => {
+    return (
+        <fragment>
+            <h3>Planet list</h3>
+            <Planet/>
+        </fragment>
+    )
+}
+
+export default Planets
---
+++ src/components/planets/planet/index.js
@@ -0,0 +1,14 @@
+import React, { fragment } from "react";
+import  './style.css'
+
+const Planet = () => {
+    return (
+        <fragment>
+            <h4>Mercurio</h4>
+            <p>Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum</p>
+            <img class='gray-img' src="https://ichef.bbci.co.uk/news/800/cpsprodpb/BA64/production/_109261774_mercurio.jpg"></img>
+        </fragment>
+    )
+}
+
+export default Planet
